4cf801d8dfc831329c6de983b00c5c2c70f83cab
Rob Savoye <rob.savoye@linaro.org>
2015-09-10 14:26:45 -0600

    Use fetech and extract directly since files have to be copied to destdir
    Change-Id: Id3e5ec9737bd866aa09a27669ed065b0acbeda44

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -90,7 +90,8 @@ checkout_infrastructure()
     # GDB now has python support, for mingw we have to download a
     # pre-built win2 binary that works with mingw32.
     if test `echo ${host} | grep -c mingw` -eq 1; then
-	files="${files} python-2.7.4-mingw32.tar.xz"
+	fetch infrastructure/python-2.7.4-mingw32.tar.xz
+	extract infrastructure/python-2.7.4-mingw32.tar.xz
 	# The mingw package of python contains a script used by GDB to
 	# configure itself, this is used to specify that path so we don't
 	# have to modify the GDB configure script.
