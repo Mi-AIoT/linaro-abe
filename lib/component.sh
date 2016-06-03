8f24d85f244553fd4ba0c75fd52fd1a6b141b637
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2016-06-03 18:11:38 +0100

    Move version configuration for gcc5-based toolchains to a dedicated dir
    This patch adds logic to source .conf snipets from config/default/ directory,
    which is a symlink to config/gcc5/ directory.  The .conf snipets define
    versions of components we are currently using.
    
    Note that configuration in config/default/ files should be limited to setting
    "latest=" field for components, and the main configuration logic should be in
    config/*.conf files.  It is OK, however, to have extra configuration logic in
    non-default config/gccN directories.
    
    An upcoming patch will add config/gcc6 directory for gcc6-based toolchains,
    and we will then switch the default to gcc6 just by updating the symlink.
    
    At that point we will be still able to easily build gcc5-based toolchains by
    adding --extraconfigdir config/gcc5 to a build.
    
    Change-Id: Icc99ee0624ba56c3f8573784e741ba344110b410

--- lib/component.sh
+++ lib/component.sh
@@ -529,6 +529,17 @@ collect_data ()
 	. "${topdir}/config/${component}.conf"
     fi
 
+    local default_conf="${topdir}/config/default/${component}.conf"
+    if test -e "$default_conf"; then
+	if grep -qv "^latest=" "$default_conf" \
+		|| ! grep -q "^latest=" "$default_conf"; then
+	    error "$default_conf should have only \"latest=\" and nothing else"
+	fi
+
+	notice "Sourcing config: $default_conf"
+	. "$default_conf"
+    fi
+
     local this_extraconfig
     for this_extraconfig in ${extraconfig[${component}]}; do
 	if test -e "${this_extraconfig}"; then
