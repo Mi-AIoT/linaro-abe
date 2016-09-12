gtalusan/abe

commit 8cdd21271d6b864762c69ad9fea1176fc6170948

Fix git pull failure when building from tag.
If a component is configured to build a tag rather than a branch, then
checkout() causes this error:
    You are not currently on a branch. Please specify which
    branch you want to merge with. See git-pull(1) for details.

Since commit 04adeb37 (Report unhandled errors), this causes the build
to fail, as it is treated as an unhandled error. The fix is to only
try git pull when an upstream branch is configured.

Change-Id: Id52314ba4c6f55705040d6eb08422ba0d8f38cd5

lib/checkout.sh
@@ -241,9 +241,9 @@ checkout()
 	    elif test x"${supdate}" = xyes; then
 		# Some packages allow the build to modify the source directory and
 		# that might screw up abe's state so we restore a pristine branch.
-		notice "Updating sources for ${component} in ${srcdir}"
-		local current_branch="`cd ${srcdir} && git branch`"
-		if test "`echo ${current_branch} | grep -c local_`" -eq 0; then
+		# If an upstream branch is configured, try to pull from it
+		if git rev-parse @{upstream} >&/dev/null; then
+		    notice "Updating sources for ${component} in ${srcdir}"
 		    dryrun "(cd ${srcdir} && git stash --all)"
 		    dryrun "(cd ${srcdir} && git reset --hard)"
 		    dryrun "(cd ${srcdir} && git_robust pull)"
@@ -262,6 +262,8 @@ checkout()
 		    # directory)
 		    dryrun "(cd ${srcdir} && git stash --all)"
 		    dryrun "(cd ${srcdir} && git reset --hard)"
+                else
+		    notice "No upstream branch for ${component} in ${srcdir}"
 		fi
 		if test x"${revision}" != x""; then
 		    # No need to pull.  A commit is a single moment in time

Mi-AIoT/linaro-abe

commit d83dbddee55c5936bbc598dcd69ebbbe96d9493c

Fix erroneous check for upstream branch
A bug was introduced in 8cdd21271d6b864762c69ad9fea1176fc6170948
(Fix git pull failure when building from tag) which causes abe to
incorrectly fail to detect that a branch has a configured upstream
branch. Fix by doing git revparse in the correct directory.

Change-Id: I8032f069456c949b6c76a4cd02526c38d6bce5a4

lib/checkout.sh
@@ -242,7 +242,7 @@ checkout()
 		# Some packages allow the build to modify the source directory and
 		# that might screw up abe's state so we restore a pristine branch.
 		# If an upstream branch is configured, try to pull from it
-		if git rev-parse @{upstream} >&/dev/null; then
+		if cd ${srcdir} && git rev-parse @{upstream} >&/dev/null; then
 		    notice "Updating sources for ${component} in ${srcdir}"
 		    dryrun "(cd ${srcdir} && git stash --all)"
 		    dryrun "(cd ${srcdir} && git reset --hard)"
@@ -283,7 +283,7 @@ checkout()
 		    fi
 		    dryrun "(cd ${srcdir} && git_robust checkout -B ${branch} origin/${branch})"
 		    if test $? -gt 0; then
-			error "Can't checkout ${revision}"
+			error "Can't checkout ${branch}"
 			return 1
 		    fi
 		    dryrun "(cd ${srcdir} && git_robust pull)"