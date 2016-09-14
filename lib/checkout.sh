0ad215040a01d91458bd413274ff736f1828bf60
Charles Baylis <charles.baylis@linaro.org>
2016-09-14 13:12:19 +0100

    Use separate code paths for updating branches, revisions and tags.
    This patch should not change the behaviour for branches and revisions, and
    introduces a new code path when a tag needs to be updated. Currently, the
    latter is minimal, and reports an error if a tag is altered.
    
    Change-Id: I00999dd80c38a5f9f5bf376e0cf555bcfeba993a

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -161,6 +161,33 @@ update_checkout_branch()
     dryrun "(cd ${srcdir} && git reset --hard)"
 }
 
+update_checkout_tag()
+{
+    local component="$1"
+    local srcdir=
+    srcdir="`get_component_srcdir ${component}`" || return 1
+    local branch=
+    branch="`get_component_branch ${component}`" || return 1
+    if cd ${srcdir} && git rev-parse origin/${branch} >& /dev/null; then
+	error "Unexpectedly not tracking origin/${branch}"
+	return 1
+    fi
+    dryrun "git_robust fetch"
+    if test $? -gt 0; then
+	error "Can't reset to ${branch}"
+	return 1
+    fi
+    local currev="`cd ${srcdir} && git rev-parse HEAD`"
+    local tagrev="`cd ${srcdir} && git rev-parse ${branch}`"
+    if test x${currev} != x${tagrev}; then
+	dryrun "git stash && git reset --hard ${branch}"
+        if test $? -gt 0; then
+	    error "Can't reset to ${branch}"
+	    return 1
+        fi
+    fi
+    return 0
+}
 # This gets the source tree from a remote host
 # $1 - This should be a service:// qualified URL.  If you just
 #       have a git identifier call get_URL first.
@@ -267,8 +294,9 @@ checkout()
 		fi
 		new_srcdir=true
 	    elif test x"${supdate}" = xyes; then
-		# Some packages allow the build to modify the source directory and
-		# that might screw up abe's state so we restore a pristine branch.
+		# Some packages allow the build to modify the source directory
+		# and that might screw up abe's state so we restore a pristine
+		# branch.
 		# If an upstream branch is configured, try to pull from it
 		if cd ${srcdir} && git rev-parse @{upstream} >&/dev/null; then
 		    update_checkout_branch ${component}
@@ -276,21 +304,6 @@ checkout()
 			error "Error during update_checkout_branch."
 			return 1
 		    fi
-                else
-		    notice "No upstream branch for ${component} in ${srcdir}"
-		fi
-		if test x"${revision}" != x""; then
-		    # No need to pull.  A commit is a single moment in time
-		    # and doesn't change.
-		    dryrun "(cd ${srcdir} && git_robust checkout -B local_${revision})"
-		    if test $? -gt 0; then
-			error "Can't checkout ${revision}"
-			return 1
-		    fi
-		else
-		    # Make sure we are on the correct branch.
-		    # This is a no-op if $branch is empty and it
-		    # just gets master.
 		    if test x"${branch}" = x; then
 			error "No branch name specified!"
 			return 1
@@ -301,6 +314,18 @@ checkout()
 			return 1
 		    fi
 		    dryrun "(cd ${srcdir} && git_robust pull)"
+                elif test x"${revision}" != x""; then
+		    notice "Building explicit revision for ${component}."
+		    # No need to pull.  A commit is a single moment in time
+		    # and doesn't change.
+		    dryrun "(cd ${srcdir} && git_robust checkout -B local_${revision})"
+		    if test $? -gt 0; then
+			error "Can't checkout ${revision}"
+			return 1
+		    fi
+		else 
+		    notice "No upstream branch for ${component} in ${srcdir}, assuming ${branch} is a tag."
+		    update_checkout_tag "${component}"
 		fi
 		new_srcdir=true
 	    fi
