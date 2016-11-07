c086b15b3fdb76ccd621c349a9b25fb10c3b262b
Christophe Lyon <christophe.lyon@linaro.org>
2016-11-07 11:53:40 +0100

    testsuite/test.sh: Use stable branch instead of staging.
    The removal of the 'staging' branch made abe's testsuite
    fail for no real reason. Use 'stable' instead, which is
    likely to stay forever...
    
    Change-Id: I07fd643270ed856ae417874b5850dd96fb7814fd

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -741,7 +741,7 @@ test_checkout "${should}" "${testing}" "${package}" "${branch}" "${revision}"
 
 testing="checkout: http://git@<url>/<repo>.git~<branch> should pass with appropriate notice"
 package="abe.git"
-branch='staging'
+branch='stable'
 revision=""
 should="pass"
 test_checkout "${should}" "${testing}" "${package}" "${branch}" "${revision}"
