1df4bf8425a3d86b5b090dd89428d99c8948bcea
Charles Baylis <charles.baylis@linaro.org>
2017-03-14 22:32:36 +0000

    Remove git ls-remote check from checkout()
    The purpose of the git ls-remote is to check the URL for
    validity. Since stderr is directed to /dev/null, a failure
    of this check is hard to debug. The check is unnecessary,
    as the URL is checked later by git clone, so it can be
    removed.
    
    Change-Id: Ia03bf176b78c72ecbba73594e48d87dcc42b5260

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -190,12 +190,6 @@ checkout()
         return 0
     fi
 
-    dryrun "git ls-remote ${repodir} > /dev/null 2>&1"
-    if test $? -ne 0; then
-	error "proper URL required"
-	return 1
-    fi
-
     case ${protocol} in
 	git*|http*|ssh*)
 	    # If the master branch doesn't exist, clone it. If it exists,
