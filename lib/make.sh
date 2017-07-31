71f2c3c667bbf72396749b6fc5026a6210252572
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-07-31 11:27:19 +0000

    Try harder in detecting architecture for kernel headers.
    Change-Id: If3cc9537d5b48c84ddf8cd5385ae644780c1e9d2

--- lib/make.sh
+++ lib/make.sh
@@ -521,24 +521,10 @@ make_install()
         local srcdir="$(get_component_srcdir ${component}) ${2:+$2}"
 	local ARCH=
 	case ${target} in
-	    *aarch64*)
-		ARCH=arm64
-		;;
-	    *i?86*)
-		ARCH=i386
-		;;
-	    *x86_64*)
-		ARCH=x86_64
-		;;
-	    *arm*)
-		ARCH=arm
-		;;
-	    *powerpc*|*ppc*)
-		ARCH=powerpc
-		;;
-	    *)
-		error "Unknown arch for make headers_install!"
-		return 1
+	    "aarch64"*) ARCH=arm64 ;;
+	    *i?86*) ARCH=i386 ;;
+	    "ppc"*) ARCH=powerpc ;;
+	    *) ARCH="${target%%-*}" ;;
 	esac
         dryrun "make ${make_opts} -C ${srcdir} headers_install ARCH=${ARCH} INSTALL_HDR_PATH=${sysroots}/usr"
         if test $? != "0"; then
