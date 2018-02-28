05f790896d16b4e88a53cd0d4fa760e599817350
Christophe Lyon <christophe.lyon@linaro.org>
2018-02-28 20:15:29 +0000

    Add --send-results-to option.
    This option is useful to send the GCC validation results via email.
    
    Change-Id: Ib7b21ace588adbedbfa7fa29d666f13a61f2f9b3

--- lib/make.sh
+++ lib/make.sh
@@ -827,6 +827,14 @@ make_check()
 
         if test x"${component}" = x"gcc"; then
             rm -rf ${sysroots}/etc/ld.so.cache
+	    # If the user provided send_results_to, send the results
+	    # via email
+	    if [ x"$send_results_to" != x ]; then
+		pushd ${builddir}
+		local srcdir="$(get_component_srcdir ${component})"
+		${srcdir}/contrib/test_summary -t -m ${send_results_to} | sh
+		popd
+	    fi
 	fi
     fi
 
